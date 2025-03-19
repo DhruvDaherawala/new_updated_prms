519028f151ab7a108d86997f40319c3d3f3ab0bf
Dhruv <dhruvdaherawala1533@gmail.com>
2025-03-19 15:02:04 +0530

    backend issue solving

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,10 +6,20 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  port: process.env.DB_PORT,
   waitForConnections: true,
   // connectionLimit: 10,
   queueLimit: 0,
 });
 
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to MySQL database");
+    connection.release();
+  }
+});
+
 module.exports = pool;
+
